780e6ef70f482e50ded984493cdbcf756c109fec
Dev <devbhardwaj621@gmail.com>
2025-07-10 17:48:32 +0530

    add more endpoints and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";//importing bcuz this keeps your route logic separate from the main app.js which helps with cleaner and scalable code
-import {registerUser} from  "../controllers/user.controller.js";//importing the actual controller function (registerUser) that contains the logic for what should happen when someone hits the /register endpoint
+// import {registerUser} from  "../controllers/user.controller.js";//importing the actual controller function (registerUser) that contains the logic for what should happen when someone hits the /register endpoint
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -41,4 +54,13 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router
