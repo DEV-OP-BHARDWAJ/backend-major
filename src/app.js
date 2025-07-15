40d7089adbf3b853a8797d795f84470a577256d6
Dev <devbhardwaj621@gmail.com>
2025-07-15 21:22:53 +0530

    just added a comment

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(cors({
     credentials: true//for authentication(allows frontend to send cookies)
 }))
 
-app.use(express.json({limit: "16kb"}))//parse incoming json req and adds to req.body ensures the request body doesn’t exceed 16 kilobytes for security and performance
+app.use(express.json({limit: "16kb"}))//parse incoming json req and adds to req.body ensures the request body doesn’t exceed 16 kilobytes for security and performance ....why its needed ? bcuz it lets us access req.body in our routes
 app.use(express.urlencoded({extended: true, limit: "16kb"}))//Parses URL-encoded form data (like from HTML forms) extended: true allows nested objects
 app.use(express.static("public"))//Serves static files like HTML, CSS, images, or JavaScript from the public folder ex-public/logo.png can be accessed at http://yourserver.com/logo.png
 app.use(cookieParser())//Parses cookies from the Cookie header and makes them available under req.cookies
